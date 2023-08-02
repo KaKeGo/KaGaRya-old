3adbfe4a307c8c753328aa6f4f15898cb7a3a1d5
KaKeGo <funfcia@hotmail.com>
2023-08-02 23:34:05 +0200

    add style for profile and fix some margins in components

--- src/content/Home/Home.jsx
+++ src/content/Home/Home.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
 
-
+import './Home.css'
 
 const Home = () => {
   const isAuthenticated = useSelector((state) => state.authCheck.isAuthenticated)
   const userData = useSelector((state) => state.authCheck.userData)
 
   return (
-    <div>
-      <h3>Home</h3>
+    <div className='home__container'>
       {isAuthenticated ? (
         <p>Welcome, {userData?.username || 'Anonymous'}!</p>
       ) : (
